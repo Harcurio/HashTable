2ca088b453d7675ac1794ed59e4ffdf67809fa62
Johor Jara <johorismael15@hotmail.com>
2018-10-08 02:52:56 -0500

    Add files via upload
    mini test implementation

--- HashTableCython.py
+++ HashTableCython.py
@@ -5,18 +5,20 @@ Created on Sun Oct  7 18:26:59 2018
 @author: johor
 """
 import math
+import random
 
 class HashData:
-    def __init__(self, key, value, hashToUse):
+    def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
-        self.hashToUse = hashToUse
+        
 
 class SeparateChainingHash:
-    def __init__(self, size):
+    def __init__(self, size, hashToUse):
         self.size = size
         self.table = [None] * self.size
+        self.hashToUse = hashToUse
 
     def hashDivision(self,key):
         return hash(key) % self.size
@@ -25,12 +27,21 @@ class SeparateChainingHash:
         A = 0.13
         return hash(math.floor(self.size*((key*A)%1)))
 
-    def hashUniversalInt(size, key):
+    def hashUniversalInt(self, key):
         p = 100003
         a = 10321
         b = 4993
-        return hash((((a*key)+b)% p)% size)
-
+        return hash((((a*key)+b)% p)% self.size)
+    
+    def wichHash(self,key):
+        
+        if(self.hashToUse == 1):
+            return self.hashDivision(key)
+        elif(self.hashToUse == 2):
+            return self.hashMultiplication(key)
+        elif(self.hashToUse == 3):
+            return self.hashUniversalInt(key)
+ 
 
     def reHash(self, entry, key, value):
         while entry and entry.key != key:
@@ -41,7 +52,7 @@ class SeparateChainingHash:
             prev.next = HashData(key, value)
 
     def set(self, key, value):
-        slot = self.hashDivision(key)
+        slot = self.wichHash(key)
         entry = self.table[slot]
         if(not entry):
             self.table[slot] = HashData(key, value)
@@ -49,7 +60,7 @@ class SeparateChainingHash:
             self.reHash(entry,key, value)
 
     def get(self, key):
-        hash = self.hashDivision(key)
+        hash = self.wichHash(key)
         if(not self.table[hash]):
             raise KeyError
         else:
@@ -57,3 +68,13 @@ class SeparateChainingHash:
             while(entry and entry.key != key):
                 entry = entry.next
             return entry.value
+        
+
+    
+test1 = SeparateChainingHash(100,1)
+    
+for i in range(100):
+    key = random.randint(0,999)
+    value = random.randint(0,999)
+    test1.set(key,value)
+    print(key)
